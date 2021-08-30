7086ae5b25c19318232e6735fd9035959be6aa39
Lucas Chmelik <lchmelik@cys.com.ar>
2021-08-30 01:35:08 -0300

    STC-36 Se crea estructura con React

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>Hola Mundo</h1>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );

1f137593b0c3b2d030edfe3a5538cbfad7a9b838
lucasambesi <lucasambesi@hotmail.com>
2021-09-03 23:16:56 -0300

    SS-31 Crear vista

--- src/components/views/AdminFactura/FacturaCRUD.js
+++ src/components/views/AdminFactura/FacturaCRUD.js
@@ -1,9 +1,48 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react'
+import {Table, Button, Container, Modal, ModalBody, ModalHader, FormGroup, ModalFooter} from 'react-bootstrap'
 
 export default function FacturaCRUD() {
+
+    const data= [
+        {id:1, numero:"1-0001-01", tipo:"primario"},
+        {id:2, numero:"2-0002-02", tipo:"primario"},
+        {id:3, numero:"3-0003-03", tipo:"terciario"},
+        {id:4, numero:"4-0004-04", tipo:"primario"},
+        {id:5, numero:"5-0005-05", tipo:"secundario"},
+    ];
+
+    const agregar = () => console.log("Usuario editado")
+    const eliminar = () => console.log("Usuario eliminado")
+
     return (
-        <div>
-            <h1>Factura CRUD</h1>
-        </div>
+        <>
+            <Container>
+            <br/>
+            <Button color ="primary">Ingresar factura</Button>
+            <br/>  
+            <br/>           
+            <Table>
+                <tr>
+                    <th>Id</th>
+                    <th>Numero</th>
+                    <th>Tipo</th>
+                    <th>Acciones</th>
+                </tr>                
+                <tbody>
+                    {data.map((elemento) => (
+                        <tr>
+                            <td>{elemento.id}</td>
+                            <td>{elemento.numero}</td>
+                            <td>{elemento.tipo}</td>
+                            <td><Button color="primary" onClick={agregar}>Editar</Button>{"  "}
+                            <Button color="danger" onClick={eliminar}>Eliminar</Button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+            </Container>
+        </>
     );
 }
